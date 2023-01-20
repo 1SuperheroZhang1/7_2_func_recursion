ec8ec995d502acec2932e8752e823fde086000f6
zhangcy <2304417358@qq.com>
2023-01-20 21:33:26 +0800

    C语言督学营 - 7.2 递归调用

---
+++ cmake-build-debug/CMakeFiles/7_2_func_recursion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7_2_func_recursion.exe"
+  "7_2_func_recursion.exe.manifest"
+  "7_2_func_recursion.pdb"
+  "CMakeFiles/7_2_func_recursion.dir/main.c.obj"
+  "lib7_2_func_recursion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/7_2_func_recursion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
